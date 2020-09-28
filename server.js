238a527b459c9d6cf99fe95524245bbe8f2eb602
sbayrak <34121748+sbayrak@users.noreply.github.com>
2020-09-28 17:10:45 +0300

    finished
    finished

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const path = require('path');
+
+connectDB();
+
+// Middleware
+app.use(express.json({ extended: false }));
+
+// DEFINE ROUTES
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
